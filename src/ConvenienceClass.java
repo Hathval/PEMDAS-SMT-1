c4ca5b943d6089d6b792d6761ce3da5c979164d0
Hathval <117620431+Hathval@users.noreply.github.com>
2022-11-17 10:48:26 +0700

    11/14/2022

--- src/ConvenienceClass.java
+++ src/ConvenienceClass.java
@@ -1,11 +1,8 @@
+import javax.swing.*;
 import java.util.Arrays;
 
 public class ConvenienceClass {
     public static void main(String[] args) {
-        int i,j;
-
-        for (i=0, j=1;i<8; ++i, ++j){
-            System.out.printf("%d + %d = %d\n", i, j, i+j);
-        }
+        
     }
 }
