edcefb0fbedac1431b3dbde37d0b9acf0db3d4f1
Hathval <117620431+Hathval@users.noreply.github.com>
2022-11-21 07:29:46 +0700

    11/21/2022

--- src/ConvenienceClass.java
+++ src/ConvenienceClass.java
@@ -1,8 +1,65 @@
-import javax.swing.*;
-import java.util.Arrays;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class ConvenienceClass {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        int panjang = s.nextInt();
+        int jadiNggak = panjang%2;
+        if (jadiNggak==0){
+            int status = 0;
+            System.exit(0);
+        }if (panjang>999&&panjang<1){
+            int status = 0;
+            System.exit(0);
+        }
+        int median = panjang/2 + 1;
+        float[] text = new float[panjang];
+        float copyArray[] = new float[text.length];
+        for (int i = 0; i < text.length; ++i) {
+            text[i] = s.nextFloat();
+            if (text[i]>250.0&&text[i]<100.0){
+                int status = 0;
+                System.exit(0);
+            }
+        }
+        s.close();
+        for (int i=0; i<text.length; i++)
+            copyArray[i] = text[i];
+        float terkecil = getSmallest(copyArray, panjang);
+
+        for (int k = median-1; k>0; k--){
+            for (int p = median; p <= panjang-k; p++){
+                if (text[p] <= terkecil) {
+                    System.out.println(p + 1);
+                    int status = 0;
+                    System.exit(0);
+                }
+            }
+            for (int q = median; q>=0+k; q--){
+                    if (text[q] <= terkecil) {
+                        System.out.println(q + 1);
+                        int status = 0;
+                        System.exit(0);
+                    }
+            }
+
+        }
+        System.out.println('1');
+
+    }
+
+    public static float getSmallest(float[] a, float total) {
+        float temp;
+        for (int i = 0; i < total; i++) {
+            for (int j = i + 1; j < total; j++) {
+                if (a[i] > a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    temp = a[i];
+                }
+            }
+        }
+        return a[0];
     }
 }
--- src/HackerrankFungsi.java
+++ src/HackerrankFungsi.java
@@ -5,14 +5,57 @@ public class HackerrankFungsi {
         Scanner s = new Scanner(System.in);
         int panjang = s.nextInt();
 
-        String[] text = new String[panjang];
-
-        for (int i=0; i< text.length; i++) {
-            text[i] = s.nextLine();
+        float[] text = new float[panjang];
+        
+        for (int i = 0; i < text.length; i++) {
+            text[i] = s.nextFloat();
             System.out.println(text[i]);
+        }
+        float terkecil = getSmallest(text,panjang);
 
+        System.out.println("Index position of is: " + findIndex(text,terkecil));
+    }
+    public static float getSmallest(float[] a, float total){
+        float temp;
+        for (int i = 0; i < total; i++)
+        {
+            for (int j = i + 1; j < total; j++)
+            {
+                if (a[i] > a[j])
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        return a[0];
+    }
+    public static float findIndex(float arr[], float t)
+    {
+
+        // if array is Null
+        if (arr == null) {
+            return -1;
         }
 
+        // find length of array
+        int len = arr.length;
+        int i = 0;
+
+        // traverse in the array
+        while (i < len) {
+
+            // if the i-th element is t
+            // then return the index
+            if (arr[i] == t) {
+                return i;
+            }
+            else {
+                i = i + 1;
+            }
+        }
+        return -1;
     }
-    public static int
+
 }
